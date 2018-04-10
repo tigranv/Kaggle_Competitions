ef8fdd7240cd5f936b9be97102cdc45ef6860d36
Tigran Vardanyan <vardanyant.89@gmail.com>
2018-04-10 19:21:33 +0400

    finished

--- KaggleCompetitions/Titanic/TitanicSelf.py
+++ KaggleCompetitions/Titanic/TitanicSelf.py
@@ -1,25 +1,68 @@
-# data analysis and wrangling
-import pandas as pd
+from pandas import read_table , get_dummies, concat , read_csv
 import numpy as np
-import random as rnd
-
-# visualization
-import seaborn as sns
 import matplotlib.pyplot as plt
+import csv
 
-# machine learning
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
+# =====================================================================
+test = read_csv("../KaggleCompetitions/Titanic/reducedData/test_reduced.csv", encoding='latin-1', sep=',', skipinitialspace=True, index_col=None, header=0,)
+train = read_csv("../KaggleCompetitions/Titanic/reducedData/train_reduced.csv", encoding='latin-1', sep=',', skipinitialspace=True, index_col=None, header=0)
+all = read_csv("../KaggleCompetitions/Titanic/OriginalData/train.csv", encoding='latin-1', sep=',', skipinitialspace=True, index_col=None, header=0,)
+
+targets = all.Survived
+
+test.head()
+train.head()
+targets.head()
+
+X_train = np.array(train, dtype=np.float)
+X_test = np.array(test, dtype=np.float)
+y_train = np.array(targets, dtype=np.float)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+#scaler.fit(X_train)
+#X_train = scaler.transform(X_train)
+#X_test = scaler.transform(X_test)
+
+# Some classifiers to test
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
-from sklearn.feature_selection import GenericUnivariateSelect
 
-train_df = pd.read_csv('../KaggleCompetitions/Titanic/OriginalData/train.csv')
-test_df = pd.read_csv('../KaggleCompetitions/Titanic/OriginalData/test.csv')
+# Test the DecisionTreeClassifier classifier
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+score = f1_score(y_test, classifier.predict(X_test))
+print("DecisionTreeClassifier - {}".format(score))
+
+# Test the Ada boost classifier
+classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1.0, algorithm='SAMME.R')
+classifier.fit(X_train, y_train)
+Submission_pred = classifier.predict(X_Sub)
+with open('../KaggleCompetitions/Titanic/AdaBoostsubmission.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    spamwriter.writerow(['PassengerId'] + ['Survived'])
+    for i in range(len(Submission_pred)):
+        spamwriter.writerow([892 + i] + [int(Submission_pred[i])])
+
+
+
+# Test Random Forest
+
+random_forest = RandomForestClassifier(n_estimators=100, max_features='sqrt')
+random_forest.fit(X_train, y_train)
+
+Submission_pred = random_forest.predict(X_Sub)
+with open('../KaggleCompetitions/Titanic/gender_submissionInAlldata.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    spamwriter.writerow(['PassengerId'] + ['Survived'])
+    for i in range(len(Submission_pred)):
+        spamwriter.writerow([892 + i] + [int(Submission_pred[i])])
+
+
+
+
+
 
